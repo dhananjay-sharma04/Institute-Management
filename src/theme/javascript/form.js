1a15b180170bc235335c3c91837fb2dc9c1afd21
DhanuXXX <dhananjayrock302@gmail.com>
2022-04-22 23:47:16 +0530

    Update form.js

--- src/theme/javascript/form.js
+++ src/theme/javascript/form.js
@@ -53,6 +53,8 @@ const validate = () => {
     }
     else if(nameVal.length <= 5){
         setErrorMsg(name, 'Name should be 5 letter');
+    }else if(/[^a-zA-Z0-9\-\/]/.test(nameVal)){
+        setErrorMsg(name, 'Name letter');
     }
     else{
         setSuccessMsg(name);
